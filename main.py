74273aab831343ce6d31fbdc57c6ee60cff018b9
orestispanago <orestispanago@gmail.com>
2022-07-03 19:04:17 +0300

    Logging info + traceback

--- main.py
+++ main.py
@@ -4,8 +4,25 @@ import datetime
 import itertools
 from pycampbellcr1000 import CR1000
 from pycampbellcr1000.utils import ListDict
-from uploader import upload_to_ftp, archive_uploaded
+from uploader import upload_to_ftp, archive_past_days
 from config import *
+import logging
+
+
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+logging.basicConfig(
+    filename="logfile.log",
+    encoding="utf-8",
+    level=logging.INFO,
+    format="%(asctime)s %(name)s %(funcName)s() - %(levelname)s: %(message)s",
+)
+logging.getLogger("pycampbellcr1000").setLevel(logging.CRITICAL + 1)
+logging.getLogger("pylink").setLevel(logging.CRITICAL + 1)
+
+logger = logging.getLogger(__name__)
 
 
 def records_to_csv(data, fname):
@@ -51,16 +68,29 @@ def get_start_stop():
     stop = datetime.datetime.utcnow()
     return start, stop
 
-start, stop = get_start_stop()
-device = CR1000.from_url("serial:/dev/ttyUSB0:115200")
-data = device.get_data("Table1", start, stop)
 
-last_record = data[-1]["Datetime"]
+def main():
+    start, stop = get_start_stop()
 
-store_to_daily_files(data)
-store_last_record(last_record, LAST_REC_FILE, DT_FORMAT)
+    logger.info("Connecting to device...")
+    device = CR1000.from_url("serial:/dev/ttyUSB0:115200")
+    logger.info("Connection successfull. Retrieving data...")
+
+    data = device.get_data("Table1", start, stop)
+    logger.info(f"Retrieved {len(data)} records.")
+
+    store_to_daily_files(data)
+    store_last_record(data[-1]["Datetime"], LAST_REC_FILE, DT_FORMAT)
+
+    local_files = sorted(glob.glob("*.csv"))
+    upload_to_ftp(local_files, FTP_IP, FTP_USER, FTP_PASS, FTP_DIR)
+    archive_past_days(local_files, "archive")
+
+    logger.info("SUCCESS! Bye...")
 
 
-local_files = sorted(glob.glob("*.csv"))
-upload_to_ftp(local_files, FTP_IP, FTP_USER, FTP_PASS, FTP_DIR)
-archive_uploaded(local_files, ARCHIVE_DIR)
+if __name__ == "__main__":
+    try:
+        main()
+    except:
+        logger.error("uncaught exception: %s", traceback.format_exc())
--- uploader.py
+++ uploader.py
@@ -1,13 +1,19 @@
 import os
 from ftplib import FTP
+import logging
+from threading import local
+
+logger = logging.getLogger(__name__)
 
 
 def upload_to_ftp(local_files, ftp_ip, ftp_user, ftp_password, ftp_dir):
+    logger.info("Uploading to FTP server...")
     with FTP(ftp_ip, ftp_user, ftp_password) as ftp:
         ftp.cwd(ftp_dir)
         for local_file in local_files:
             with open(local_file, "rb") as f:
                 ftp.storbinary(f"STOR {local_file}", f)
+    logger.info(f"Uploaded {len(local_files)} files.")
 
 
 def mkdir_if_not_exists(dir_path):
@@ -15,8 +21,10 @@ def mkdir_if_not_exists(dir_path):
         os.makedirs(dir_path)
 
 
-def archive_uploaded(local_files, dest_folder):
+def archive_past_days(local_files, dest_folder):
     mkdir_if_not_exists(dest_folder)
     if len(local_files) > 1:
         for local_file in local_files[:-1]:
-            os.rename(local_file, f"{dest_folder}/{local_file}")
+            new_fname = f"{dest_folder}/{local_file}"
+            os.rename(local_file, new_fname)
+            logger.info(f"Renamed file {local_file} to {new_fname}")
